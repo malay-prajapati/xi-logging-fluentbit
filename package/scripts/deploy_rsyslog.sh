d077e882e7b4f1f088e43f4d58dd5b5ffdc39a0d
Pranesh Neelakrishnan <pranesh.neelakrishna@nutanix.com>
2019-09-05 14:58:19 -0700

    rsyslog needs changes

--- package/scripts/deploy_rsyslog.sh
+++ package/scripts/deploy_rsyslog.sh
@@ -1,34 +1,15 @@
 # !/bin/bash
 DIR="/etc/rsyslog.d"
-RSYSLOG_PID_DIR="/home/nutanix/RSYSLOG"
-PID_FILE="/home/nutanix/RSYSLOG/deploy_rsyslog.sh.pid"
 
 #Handle previously running processes for the curent script
-if [[ -e $PID_FILE]]; then
-    for pid in $(pidof -x deploy_rsyslog.sh); do
-        if [ $pid != $$ ]; then
-            echo "killing the already running process with PID $pid"
-            `kill $pid`
-            if [ "$?" -ne "0" ]; then
-                echo "Process is not getting terminated" 1>&2
-                exit 1
-            fi
-    # clean up the contents of the previously existing dir
-    `rm -rf $RSYSLOG_PID_DIR`
-fi
-
-if [[ ! -e $RSYSLOG_PID_DIR ]]; then
-    mkdir $RSYSLOG_PID_DIR
-elif [[ -d $RSYSLOG_PID_DIR ]]; then
-    echo "$RSYSLOG_PID_DIR already exists already." 1>&2
-    exit 1
-fi
-
-# Delete pidfile during system exits
-trap "rm -f -- '$PID_FILE'" EXIT
-
-# Save the process id in the pid file
-echo $$ > "$PID_FILE"
+for pid in $(ps -aux | less | grep -i rsyslo[g] | awk {'print $2'}); do
+    echo "killing the already running process with PID $pid"
+    `kill $pid`
+    if [ "$?" -ne "0" ]; then
+        echo "Process is not getting terminated" 1>&2
+        exit 1
+    fi
+done
 
 # Touch rsyslog config file
 `/usr/bin/touch $DIR/60-fluent-bit.conf`
